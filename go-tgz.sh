617b4f9b10c84903204fa49fc6ad13b0023bbda5
Gérald QUINTANA <gerald-externe.quintana@erdf.fr>
2017-03-21 15:43:25 +0100

    Improve packaging

--- go-tgz.sh
+++ go-tgz.sh
@@ -10,6 +10,10 @@ mkdir ${workdir}
 mkdir ${workdir}/bin
 mkdir ${workdir}/config
 cp $GOPATH/bin/Burrow ${workdir}/bin/burrow
+chmod -R 0755 ${workdir}/bin
 cp config/* ${workdir}/config
+chmod 0755 ${workdir}/config
+chmod 0644 ${workdir}/config/*
+chmod 0600 ${workdir}/config/user.cfg
 tar -czf burrow-$version.tar.gz ${workdir}
 rm -Rf ${workdir}
